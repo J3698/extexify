85a9d2741f59c08a3a1c0b511f530068530d7b90
Antioch Sanders <antiochsanders@gmail.com>
2021-04-23 03:53:07 -0400

    Add train val test split

--- prep_data.py
+++ prep_data.py
@@ -3,6 +3,10 @@ import torch
 import matplotlib.pyplot as plt
 import subprocess
 import os
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence
+
 
 def main():
     dataset = ExtexifyDataset("./dataX.npy", "./dataY.npy")
@@ -14,8 +18,38 @@ def main():
     plt.show()
 
 
+def collate(batch):
+    x, y = zip(*batch)
+    packed_x = pack_sequence(x, enforce_sorted = False)
+    y = torch.tensor(y)
+    return packed_x, y
+
+
+def dataloaders(batch_size):
+    download_dataset()
+    dataset = ExtexifyDataset("./dataX.npy", "./dataY.npy")
+    dataset_train, dataset_val, dataset_test = \
+        dataset.split_train_val_test(0.7, 0.2, 0.1)
+
+    dataloader_train = DataLoader(dataset_train, batch_size = batch_size,\
+                                  shuffle = True, collate_fn = collate)
+    dataloader_val = DataLoader(dataset_val, batch_size = batch_size,\
+                                shuffle = False, collate_fn = collate)
+    dataloader_test = DataLoader(dataset_test, batch_size = batch_size,\
+                                 shuffle = False, collate_fn = collate)
+
+    return dataloader_train, dataloader_val, dataloader_test
+
+
 class ExtexifyDataset(torch.utils.data.Dataset):
-    def __init__(self, strokes_file, labels_file):
+    def __init__(self, strokes_file = "trainX.npy",\
+                       labels_file = "trainY.npy",\
+                       strokes_labels = None):
+
+        if strokes_labels is not None:
+            self.strokes, self.labels = strokes_labels
+            return
+
         strokes = np.load(strokes_file, allow_pickle = True)
         labels = np.load(labels_file, allow_pickle = True)
         print("Loaded data")
@@ -33,11 +67,31 @@ class ExtexifyDataset(torch.utils.data.Dataset):
 
         assert len(self.strokes) == len(self.labels)
 
+
     def __len__(self):
         return len(self.strokes)
 
+
     def __getitem__(self, idx):
         return self.strokes[idx], self.labels[idx]
+
+
+    def split_train_val_test(self, percent_train, percent_val, percent_test):
+        total = percent_train + percent_val + percent_test
+        assert abs(total - 1) < 1e-9, total
+        assert 0 < percent_train and 0 < percent_val and 0 < percent_test
+
+        train, rest = self.split(percent_train)
+        val, test = rest.split(percent_val / (percent_test + percent_val))
+        return train, val, test
+
+    def split(self, percent_train):
+        x_train, x_test, y_train, y_test = \
+            train_test_split(self.strokes, self.labels, \
+                             train_size = percent_train)
+        train = ExtexifyDataset(strokes_labels = [x_train, y_train])
+        test = ExtexifyDataset(strokes_labels = [x_test, y_test])
+        return train, test
 
 
 def download(filename, drivelink):
@@ -54,6 +108,7 @@ def download(filename, drivelink):
         cmd = ["wget", "--load-cookies", "/tmp/cookies.txt", link, "-O", filename]
         subprocess.run(cmd)
         subprocess.run(["rm", "-rf", "/tmp/cookies.txt"])
+
 
 def download_dataset():
     download("./dataX.npy", "18KMxHJujq8Nb3SIMMFPHvTvQ3oBJtqXZ")
--- train.py
+++ train.py
@@ -1,43 +1,37 @@
 import torch
 import torch.optim as optim
 import torch.nn as nn
-from torch.utils.data import DataLoader
-from prep_data import ExtexifyDataset, download_dataset
+from prep_data import dataloaders
 from tqdm import tqdm
-from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence
 from models import *
 
 
-def collate(batch):
-    x, y = zip(*batch)
-    packed_x = pack_sequence(x, enforce_sorted = False)
-    y = torch.tensor(y)
-    return packed_x, y
-
-
-download_dataset()
 batch_size = 512 if torch.cuda.is_available() else 2
-dataset = ExtexifyDataset("./dataX.npy", "./dataY.npy")
-dataloader = DataLoader(dataset, batch_size = batch_size,\
-                        shuffle = True, collate_fn = collate)
+epochs = 10
 
-model = Model()
-if torch.cuda.is_available():
-    model.cuda()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters())
-for epoch in range(10):
+def main():
+    train_loader, val_loader, test_loader = dataloaders(batch_size)
+    model = Model()
+    if torch.cuda.is_available():
+        model.cuda()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
+
+    for e in range(epochs):
+        validate(model, val_loader)
+        train(model, optimizer, criterion, train_loader)
+
+def train(model, optimizer, criterion, train_loader):
     total = 0
     correct = 0
     correct5 = 0
     i = 0
-    bar = tqdm(dataloader)
+    bar = tqdm(train_loader)
     for x, y in bar:
         if torch.cuda.is_available():
             x = x.cuda()
             y = y.cuda()
 
-        i += 1
         optimizer.zero_grad()
 
         out = model(x)
@@ -46,16 +40,47 @@ for epoch in range(10):
         loss.backward()
         optimizer.step()
 
-        total += len(x)
-
+        total += len(y)
         c = (torch.argmax(out, dim = 1) == y).sum().item()
-
         top5 = torch.topk(out, 5, dim = 1).indices
-        assert top5.shape == (x.shape[0], 5)
         c5 = torch.any(top5 == y[:, None], dim = 1).sum().item()
-
         correct += c
         correct5 += c5
 
-        bar.set_postfix({"correct": c, "top1": correct / total,
-                         "top5": correct5 / total})
+        postfix = {"correct": c,\
+                   "top1": correct / total,\
+                   "top5": correct5 / total}
+        bar.set_postfix(postfix)
+
+@torch.no_grad()
+def validate(model, eval_loader):
+    total = 0
+    correct = 0
+    correct5 = 0
+    i = 0
+    bar = tqdm(eval_loader)
+
+    for x, y in bar:
+        if torch.cuda.is_available():
+            x = x.cuda()
+            y = y.cuda()
+
+        out = model(x)
+
+        total += len(y)
+        c = (torch.argmax(out, dim = 1) == y).sum().item()
+        top5 = torch.topk(out, 5, dim = 1).indices
+        c5 = torch.any(top5 == y[:, None], dim = 1).sum().item()
+        correct += c
+        correct5 += c5
+
+        postfix = {"correct": c,\
+                   "top1": correct / total,\
+                   "top5": correct5 / total}
+        bar.set_postfix(postfix)
+
+
+if __name__ == "__main__":
+    main()
+
+
